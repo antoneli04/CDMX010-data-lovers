a97dfed34d7288d6bbc2c7bb1117174204d0aa5f
antoneli04 <antoniali_4@hotmail.com>
2021-02-07 12:21:39 -0500

    final

--- src/main.js
+++ src/main.js
@@ -1,74 +1,114 @@
 //El proposito de main es manipular el DOM (eso incluye invocar funciones)
 //--------------------------------------------------------
-import {filterAlgo} from './data.js'
+// import {filterResults} from './data.js'
+import {filterResults} from './data.js';
+import rickandmorty from './data/rickandmorty/rickandmorty.js';
 
+//Eventos
 document.addEventListener('DOMContentLoaded',()=>{
-    getData()
+  
+    showData(information);
 })
 
-
-
-const filterSpecies = document.querySelector('#specie');
-const filterStatus = document.querySelector('#status');
-const filterGender = document.querySelector('#gender');
-const resuls = document.querySelector('#character');
-
-let content = document.getElementById('character')
+//variables
+let results = document.getElementById('character')
+let information = rickandmorty.results;
+const species = document.querySelector('#specie'),
+      status = document.querySelector('#status'),
+      gender = document.querySelector('#gender'),
+      type = document.querySelector('#type'),
+      order = document.querySelector('#order');
 
 //Event listener para los selects de la busqueda
-filterSpecies.addEventListener('change', e => {
-    searchData.species = e.target.value;
-  console.log( filterAlgo(searchData.species), 'Esto ejecuto tu filter')
-  })
-  
-  filterStatus.addEventListener('change', e => {
-    searchData.status = e.target.value;
-    console.log(searchData);
-  })
-  
-  filterGender.addEventListener('change', e => {
-    searchData.gender = e.target.value;
-    console.log(searchData);
-  })
-
-  const searchData = {
-    species: "",
-    status: "",
-    gender: "",
-}
-
-  
-function getData() {
-
-    const url ='data/rickandmorty/rickandmorty.json'
-    fetch(url)
-    .then(response => response.json ())
-    .then(alldata => {setHtml(alldata.results)})
-    .catch(error => console.log(error));
-}
-
-function setHtml(data){
-    data.forEach((character)=>{
-    const {id, image, name, status, species, type, gender, origin, location}=character;
-    const divHtml=document.createElement('div');
+species.addEventListener('change', e => {
+    // searchData.species = e.target.value;
+    information.species = e.target.value;
     
+    const result = filterResults();
+    if(result.length){
+      showData(result);
+      }else{
+        noResult();
+      }
+  })
+
+  status.addEventListener('change', e => {
+    information.status = e.target.value;
+    
+    const result = filterResults();
+    if(result.length){
+      showData(result);
+      }else{
+        noResult();
+      }
+  })
+
+  gender.addEventListener('change', e => {
+    information.gender = e.target.value;
+    const result = filterResults();
+    if(result.length){
+      showData(result);
+      }else{
+        noResult();
+      }
+  })
+
+  type.addEventListener('change', e => {
+    information.type = e.target.value;
+
+    const result = filterResults();
+    if(result.length){
+      showData(result);
+      }else{
+        noResult();
+      }
+  })
+
+  order.addEventListener('change', e => {
+    // console.log('evento de order');
+    information.order = e.target.value;
+
+   const result = filterResults(information.order);
+   if(result.length){
+     showData(result);
+     }else{
+       noResult();
+     }
+  })
+
+function showData(information){
+  CleanHTML();  //limpia el html previo
+  information.forEach( element => {
+    const divHtml = document.createElement('div');
     divHtml.className = 'card';
-    
         divHtml.innerHTML=`
-        <img src='${image}'/><br><br>
-        <p> <b>Id</b>: ${id}</P>
-        <p> <b>Name:</b> ${name}</P>
-        <p> <b>Status:</b> ${status}</P>
-        <p> <b>Species:</b> ${species}</P>
-        <p> <b>Type:</b> ${type}</P>
-        <p> <b>Gender:</b> ${gender}</P>
-        <p> <b>Origin:</b> ${origin.name}</P>
-        <p> <b>Location:</b> ${location.name}</P>
+        <img src='${element.image}'/><br><br>
+        <p> <b>Id</b>: ${element.id}</P>
+        <p> <b>Name:</b> ${element.name}</P>
+        <p> <b>Status:</b> ${element.status}</P>
+        <p> <b>Species:</b> ${element.species}</P>
+        <p> <b>Type:</b> ${element.type}</P>
+        <p> <b>Gender:</b> ${element.gender}</P>
+        <p> <b>Origin:</b> ${element.origin.name}</P>
+        <p> <b>Location:</b> ${element.location.name}</P>
         `;
-    content.appendChild(divHtml)
-    })
-
-   
-    console.log(data);
+    results.appendChild(divHtml)
+  })
 }
+
+function CleanHTML(){
+  while(results.firstChild){
+    results.removeChild(results.firstChild);
+  }
+}
+
+function noResult(){
+  CleanHTML();
+  const noResult = document.createElement('div');
+  noResult.classList.add('alert')
+  noResult.innerHTML = "You're not gonna believe this,<br> because it ussually never happens,<br> 👽 but 👽<br> No matches found";
+  results.appendChild(noResult);
+}
+
+
 
